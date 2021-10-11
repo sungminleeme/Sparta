c1b7129a4a8fbe9827189d2679947e620f60c549
sinyubin <85838870+sinyubin@users.noreply.github.com>
2021-10-12 02:25:37 +0900

    grid

---
+++ src/elements/Button.js
@@ -0,0 +1,71 @@
+// import { BASE_OPTION_DEFAULTS } from "@fullcalendar/common";
+import React from "react";
+import styled from "styled-components";
+
+const Button = (props) => {
+  const { text, _onClick, is_float, children, margin, width, padding } = props;
+
+  if (is_float) {
+    return (
+      <React.Fragment>
+        <FloatButton onClick={_onClick}>{text ? text : children}</FloatButton>
+      </React.Fragment>
+    );
+  }
+
+  const styles = {
+    margin: margin,
+    width: width,
+    padding: padding,
+  };
+
+  return (
+    <React.Fragment>
+      <ElButton {...styles} onClick={_onClick}>
+        {text ? text : children}
+      </ElButton>
+    </React.Fragment>
+  );
+};
+
+Button.defaultProps = {
+  text: false,
+  children: null,
+  _onClick: () => {},
+  is_float: false,
+  margin: false,
+  width: "100%",
+  padding: "12px 0px",
+};
+
+const ElButton = styled.button`
+  width: ${(props) => props.width};
+  background-color: #212121;
+  color: #fff;
+  padding: ${(props) => props.padding};
+  box-sizing: border-box;
+  border: none;
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+`;
+
+const FloatButton = styled.button`
+  width: 50px;
+  height: 50px;
+  background-color: #212121;
+  color: #fff;
+  padding: 16px;
+  box-size: border-box;
+  font-size: 36px;
+  font-weight: 800;
+  position: fixed;
+  bottom: 50px;
+  right: 16px;
+  text-align: center;
+  border: none;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Button;
